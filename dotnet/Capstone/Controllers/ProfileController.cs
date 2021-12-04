3193a767a73030d4a97fbaa7e3f821ab023e6b37
Zach Laird <zlaird5454@gmail.com>
2021-12-04 11:40:11 -0500

    working on ProfileDao/Controller, commiting to grab server update from main

---
+++ dotnet/Capstone/Controllers/ProfileController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Capstone.DAO;
+using Capstone.Models;
+using Capstone.Security;
+
+namespace Capstone.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProfileController : Controller //controller vs controllerbase
+    {
+        private readonly IProfileDao profilesDao;
+
+        [HttpPost("/")]
+        IActionResult AddProfile(Profile profile)
+        {
+            return null;
+        }
+
+        [HttpPut("/{userID}")]
+        IActionResult UpdateProfile(int userID, Profile profile)
+        {
+            return null;
+        }
+
+        [HttpDelete("/{userID}")]
+        IActionResult DeleteProfile(int id)
+        {
+            return null;
+        }
+
+        [HttpGet("/{userID}")]
+        IActionResult GetProfile(int userID)
+        {
+            return null;
+        }
+
+        [HttpGet("/")]
+        IActionResult GetAllProfiles()
+        {
+            return null;
+        }
+
+    }
+}
---
+++ dotnet/Capstone/DAO/ProfileSqlDao.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Capstone.Models;
+
+namespace Capstone.DAO
+{
+    public class ProfileSqlDao : IProfileDao
+    {
+        private readonly string connectionString;
+        private Profile returnProfile = new Profile();
+
+        public ProfileSqlDao (string dbConnectionString)
+        {
+            connectionString = dbConnectionString;
+        }
+
+        public Profile GetProfile(int userID)
+        {
+            using(SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("SELECT profile FROM")
+            }
+        }
+
+        public List<Profile> GetAllProfiles()
+        {
+            return profile;//remove later
+        }
+
+        public Profile AddProfile(Profile profile)
+        {
+            return profile;//remove later
+        }
+
+        public Profile UpdateProfile(int userID)
+        {
+            return profile;//remove later
+        }
+
+        public Profile DeleteProfile(int userID)
+        {
+            return profile;//remove later
+        }
+    }
+}
