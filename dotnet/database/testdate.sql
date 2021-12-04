18d582bfd73269eca7dfa286a0443a4d8c49196b
Kiran Meyers <kiranmeyers@gmail.com>
2021-12-04 17:57:30 -0500

    add playdates to test data

--- dotnet/database/testdate.sql
+++ dotnet/database/testdate.sql
@@ -1,15 +1,13 @@
 
-
-
+USE final_capstone
+GO
 
 DELETE user_profile
-
 INSERT INTO user_profile (user_id,first_name,last_name,zip_code)
 VALUES (1,'test','testerson',41000),
 (2,'admin','adminson',41000)
 
 DELETE pet_profile
-
 INSERT INTO pet_profile (pet_name,animal_type,breed,age,size,is_male,is_spayed_neutered,description)
 VALUES ('doggo1','dog','husky',5,3,1,1,'doggo description 1'),
 ('doggo2','dog','shiba inu',1,1,0,0,'doggo description 2'),
@@ -18,6 +16,12 @@ VALUES ('doggo1','dog','husky',5,3,1,1,'doggo description 1'),
 --SELECT * FROM pet_profile
 
 INSERT INTO users_pets (user_id,pet_id)
-values (1,1),(2,2),(2,3)
+VALUES (1,1),(2,2),(2,3)
 
 --SELECT * FROM users_pets
+
+INSERT INTO play_dates (host_user_id,host_pet_id,guest_pet_id,date_time)
+VALUES (1,1,2,GETDATE()),
+(2,2,1,GETDATE())
+
+--SELECT * FROM play_dates
