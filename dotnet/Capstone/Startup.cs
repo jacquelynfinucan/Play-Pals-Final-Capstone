92e78e410e5704fae672272190f9c7b56e14587d
Jacquelyn Finucan <jacquelynfinucan@gmail.com>
2021-12-04 12:54:13 -0500

    Merged with Zach's & added dependency injection for IPlayDateDao/PlayDateSqlDao

--- dotnet/Capstone/Startup.cs
+++ dotnet/Capstone/Startup.cs
@@ -64,6 +64,7 @@ namespace Capstone
             services.AddSingleton<IPasswordHasher>(ph => new PasswordHasher());
             services.AddTransient<IUserDao>(m => new UserSqlDao(connectionString));
             services.AddTransient<IProfileDao>(m => new ProfileSqlDao(connectionString));
+            services.AddTransient<IPlayDateDao>(m => new PlayDateSqlDao(connectionString));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
