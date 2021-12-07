db9632ed9768712ea0c5fbc8e6a30ccc07057da4
Kiran Meyers <kiranmeyers@gmail.com>
2021-12-07 16:40:29 -0500

    basic playdate cards

--- vue/src/components/PlayDateCard.vue
+++ vue/src/components/PlayDateCard.vue
@@ -1,10 +1,10 @@
 <template>
-   <div id="main" style="background-color:blue;">
+   <div id="main">
 
-    <h3>Host Name: {{playdate.hostName}}</h3>
+    <h3 id="header">Host Name: {{playdate.hostName}}</h3>
     <br/>
-    <h3>Host Pet Name: {{playdate.hostPet}}</h3>
-    <h3>Guest Pet Name: {{playdate.guestPet}}</h3>
+    <h3 class="pet-name">Host Pet Name: {{playdate.hostPet}}</h3>
+    <h3 class="pet-name">Guest Pet Name: {{playdate.guestPet}}</h3>
 
 
 
@@ -23,8 +23,42 @@ export default {
 }
 </script>
 
-<style>
- /* #main {
-     display:grid;
- } */
+<style scoped>
+#main {
+  border: solid 1px darkgrey;
+  border-radius: 5px;
+
+  background-color: white;
+
+  margin-bottom: 10px;
+}
+
+#header {
+  border-radius: 5px;
+  
+  background-color: lightblue;
+    margin-top:0px;
+    padding:30px;
+  display: flex;
+  flex-direction: row;
+}
+
+.pet-img {
+  border: solid 5px darkgrey;
+  border-radius: 9999px;
+  
+  width: 75px;
+  margin: 10px;
+  
+  align-self: flex-start;
+  justify-self: center;
+}
+
+.pet-name{ 
+text-align: left;
+}
+
+.pet-body {
+  padding: 10px;
+}
 </style>
