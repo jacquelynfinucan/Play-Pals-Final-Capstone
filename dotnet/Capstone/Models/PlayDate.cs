6357ba417a83a2e7011998730bbe1d3458cd202a
Kiran Meyers <kiranmeyers@gmail.com>
2021-12-13 15:08:53 -0500

    play date model

--- dotnet/Capstone/Models/PlayDate.cs
+++ dotnet/Capstone/Models/PlayDate.cs
@@ -7,18 +7,20 @@ namespace Capstone.Models
 {
     public class PlayDate
     {
-        public string title { get; set; }
-        public int PlayDateID { get;set; }
-        public int HostUserID { get; set; }
+        public string title { get; set; } //required
+        public int PlayDateID { get;set; } //required
+        public int HostUserID { get; set; } //required
+        public int HostPetID { get; set; } //required
+        public int GuestPetID { get; set; } = -1;  //required
+
+        public string Address { get; set; } //required
+        public DateTime DateOfPlayDate { get; set; } //required
         public string HostUsername { get; set; }
-        public int HostPetID { get; set; }
         public string HostPetName { get; set; }
-        public int GuestPetID { get; set; } = -1;
         public string GuestUsername { get; set; }
         public string GuestPetName { get; set; }
         //model to be created
-        public string location_id { get; set; }
-        public DateTime DateOfPlayDate { get; set; }
+        public string location_id { get; set; } 
     }
 
     public class PlayDateThread
