9bdf3323783c83a77326b274941c66b80a8a8ccb
Mark Spieth <mark.t.spieth@gmail.com>
2021-12-14 09:39:47 -0500

    Updated how dates are displayed for play dates and messages

--- vue/src/components/PlayDateCard.vue
+++ vue/src/components/PlayDateCard.vue
@@ -2,7 +2,7 @@
    <div id="main">
     <h2 id="header">Playdate Title: {{playdate.title}}</h2>
     <div id="card-body">
-    <h3 class="date-time">Date and Time: {{playdate.dateOfPlayDate}}</h3>
+    <h3 class="date-time">Date and Time: {{formatedDate}}</h3>
     <h3 class ="host-name">Host User: {{playdate.hostUsername}}</h3>
     <h3 class="pet-name">Host Pet: {{playdate.hostPetName}}</h3>
     <h3 class="pet-name">Guest User: {{playdate.guestUsername}}</h3>
@@ -26,6 +26,12 @@ export default {
         currentUserIsHost: false,
       }
     },
+    computed: {
+      formatedDate() {
+        let date = new Date(this.playdate.dateOfPlayDate);        
+        return date.toLocaleString();
+      }
+    }
 }
 </script>
 
