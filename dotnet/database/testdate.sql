a894cc4ab1f74682226acb79935d9293bd32b658
Jacquelyn Finucan <jacquelynfinucan@gmail.com>
2021-12-16 11:13:30 -0500

    Building out test data in database

--- dotnet/database/testdate.sql
+++ dotnet/database/testdate.sql
@@ -3,58 +3,66 @@ GO
 
 --populate test users with a default password of "password"
 INSERT INTO users (username, password_hash, salt, user_role) 
-VALUES ('User','Jg45HuwT7PZkfuKTz6IB90CtWY4=','LHxP4Xh7bN0=','user'),	--user_id = 1
-	('Admin','YhyGVQ+Ch69n4JMBncM4lNF/i9s=', 'Ar/aB2thQTI=','admin'),	--user_id = 2
+VALUES ('Alex','Jg45HuwT7PZkfuKTz6IB90CtWY4=','LHxP4Xh7bN0=','user'),	--user_id = 1 user
+	('Chelsea','YhyGVQ+Ch69n4JMBncM4lNF/i9s=', 'Ar/aB2thQTI=','admin'),	--user_id = 2 admin
 	('Mark','Jg45HuwT7PZkfuKTz6IB90CtWY4=','LHxP4Xh7bN0=','user'),		--user_id = 3
 	('Zach','Jg45HuwT7PZkfuKTz6IB90CtWY4=','LHxP4Xh7bN0=','user'),		--user_id = 4
 	('Ryan','Jg45HuwT7PZkfuKTz6IB90CtWY4=','LHxP4Xh7bN0=','user'),		--user_id = 5
-	('Jacque','Jg45HuwT7PZkfuKTz6IB90CtWY4=','LHxP4Xh7bN0=','user'),	--user_id = 6
+	('Jacquelyn','Jg45HuwT7PZkfuKTz6IB90CtWY4=','LHxP4Xh7bN0=','user'),	--user_id = 6
 	('Kiran','Jg45HuwT7PZkfuKTz6IB90CtWY4=','LHxP4Xh7bN0=','user'),		--user_id = 7
 	('David','Jg45HuwT7PZkfuKTz6IB90CtWY4=','LHxP4Xh7bN0=','user');		--user_id = 8
 
 --populate test profiles for the above users
 INSERT INTO user_profile (user_id, first_name, last_name, email, zip_code) 
-VALUES (1, 'userFirst', 'userLast', 'user@gmail.com', 41000),
-	(2, 'adminFirst', 'adminLast', 'admin@gmail.com', 41000),
+VALUES (1, 'Alex', 'Moon', 'alex@gmail.com', 42114), --user
+	(2, 'Chelsea', 'Cooper', 'chelsea@gmail.com', 41105), --admin
 	(3, 'Mark', 'Spieth', 'mark@gmail.com', 41000),
-	(4, 'Zach', 'Laird', 'zach@gmail.com', 41000),
+	(4, 'Zach', 'Laird', 'zach@gmail.com', 44110),
 	(5, 'Ryan', 'Garro', 'ryan@gmail.com', 41000),
-	(6, 'Jacquelyn', 'Finucan', 'jacquelyn@gmail.com', 41000),
-	(7, 'Kiran', 'Meyers', 'kirian@gmail.com', 41000),
-	(8, 'David', 'Ferreira', 'david@gmail.com', 41000);
+	(6, 'Jacquelyn', 'Finucan', 'jacquelyn@gmail.com', 44111),
+	(7, 'Kiran', 'Meyers', 'kirian@gmail.com', 44107),
+	(8, 'David', 'Ferreira', 'david@gmail.com', 41123);
 
 --populate test pets for the above users
 INSERT INTO pet_profile (pet_name, animal_type, breed, age, size, is_male, is_spayed_neutered, description)
-VALUES ('Bolt', 'Dog', 'Bulldog', 5, 3, 1, 1, 'Good boy!'),				--pet_id = 1
-	('Hunter', 'Dog', 'Poodle', 2, 2, 0, 0, 'Very energetic'),			--pet_id = 2
-	('Alpha', 'Dog', 'Beagle', 4, 1, 1, 1, 'Sleepy'),					--pet_id = 3
-	('Rain', 'Dog', 'Golden Retriever', 6, 3, 0, 0, 'Very shy'),		--pet_id = 4
-	('Ritz', 'Dog', 'German Shepherd', 1, 1, 1, 1, 'Bites!'),			--pet_id = 5
-	('Cinder', 'Dog', 'Beagle', 4, 3, 0, 0, 'Playful'),					--pet_id = 6
-	('Bixby', 'Dog', 'Poodle', 3, 2, 1, 1, 'Loud barker'),				--pet_id = 7
-	('Arrow', 'Dog', 'Bulldog', 5, 2, 0, 0, 'Loves to play fetch');		--pet_id = 8
+VALUES ('Bolt', 'Dog', 'Bulldog', 5, 1, 1, 1, 'Good boy!'),				--pet_id = 1
+	('Hunter', 'Dog', 'Poodle', 2, 3, 0, 0, 'Very energetic'),			--pet_id = 2
+	('Flopsy', 'Dog', 'Beagle', 10, 1, 1, 1, 'Well behaved and gentle'),			--pet_id = 3
+	('Raindance', 'Dog', 'Golden Retriever', 6, 3, 0, 0, 'Very shy and quiet'),		--pet_id = 4
+	('Jerry', 'Dog', 'German Shepherd', 1, 3, 1, 1, 'Loves belly rubs!'),			--pet_id = 5
+	('Cinderella', 'Dog', 'Beagle', 4, 1, 0, 0, 'Rambunctious pupper that loves bacon'),--pet_id = 6
+	('Sir Bixby', 'Dog', 'Shih Tzu', 3, 2, 1, 1, ''),						--pet_id = 7
+	('Vida', 'Dog', 'Bulldog', 5, 2, 0, 0, 'Loud barker, loves to chase squirrels'), --pet_id = 8
+	('Mrs. Squishy', 'Dog', 'Mixed', 13, 1, 0, 1, 'Fluffy and dignified, loves dressing up'), --pet_id = 9
+	('Gumby', 'Dog', 'Pug', 8, 2, 1, 0, 'Puggalicious!'),			--pet_id = 10
+	('Blue', 'Dog', 'Husky', 2, 3, 1, 1, 'Typical husky-good digger and loves to run'), --pet_id = 11
+	('Gracie', 'Dog', 'Pomeranian', 3, 1, 0, 1, '');				--pet_id = 12
 
---assign person
+--assign personality traits
 INSERT INTO pets_personality_traits (pet_id, personality_id)
 VALUES (1, 1), (1, 4), (1, 8), 
-	(2, 2), (2, 8), 
+	(2, 1), (2, 6), (2, 8), 
 	(3, 4), (3, 5), (3, 6), (3, 8), (3, 9),
-	(4, 2), (4, 1), (4, 4),
-	(5, 1), (5, 2),
-	(6, 6), (6, 7),
+	(4, 2), (4, 6), (4, 4),
+	(5, 1), (5, 9), (5, 8),
+	(6, 6), (6, 7), (6, 1),
 	(7, 7),
-	(8, 4), (8, 5), (8, 9)
+	(8, 4), (8, 5), (8, 8),
+	(9, 2), (9,8), (2,9),
+	(10, 1), (10, 4), (10, 7), (10, 9),
+	(11,1), (11,5), (11,8), (11,9),
+	(12,2), (12,4), (12,8), (12,9)
 
 --assign the test pets to users
 INSERT INTO users_pets (user_id, pet_id)
 VALUES (1,1),
-	(2,2),
+	(2,2), (2,9),
 	(3,3),
 	(4,4),
-	(5,5),
-	(6,6),
-	(7,7),
-	(8,8);
+	(5,8),
+	(6,6), (6,11),
+	(7,7), (7,10),
+	(8,5), (8,12);
 
 --add two locations	
 INSERT INTO location (location_id, location_name, latitude, longitude, formatted_address)
@@ -65,7 +73,7 @@ VALUES ('ChIJ6dnrD4_7MIgRyDZm8_T8P48', 'Wade Lagoon', 41.5060304, -81.6112139, '
 INSERT INTO play_dates (title, host_user_id, host_pet_id, guest_pet_id, date_time, address, status_id,location_id)
 VALUES ('City Park Play Date', 1, 1, 2, GETDATE(),'1234 Main St Cleveland, OH 44111', 2,'ChIJ6dnrD4_7MIgRyDZm8_T8P48'),									--play_date_id = 1
 	('At Home Play Date', 2, 2, 5, GETDATE(),'567 Brick Blvd Lakewood, OH 44107', 2,'ChIJ6dnrD4_7MIgRyDZm8_T8P48'),										--play_date_id = 2
-	('Jogging and Dogs', 3, 3, 2, GETDATE(),'1234 Main St Cleveland, OH 44111', 2,'ChIJ6dnrD4_7MIgRyDZm8_T8P48'),										--play_date_id = 3
+	('Jogging and Dogs', 2, 2, 5, GETDATE(),'1234 Main St Cleveland, OH 44111', 2,'ChIJ6dnrD4_7MIgRyDZm8_T8P48'),										--play_date_id = 3
 	('Meet and Greet', 4, 4, 1, GETDATE(),'567 Brick Blvd Lakewood, OH 44107', 2,'ChIJ6dnrD4_7MIgRyDZm8_T8P48'),											--play_date_id = 4
 	('Dog Yoga', 5, 5, 7, GETDATE(),'89 Memory Lane Brooklyn, OH 44144', 2,'ChIJ6dnrD4_7MIgRyDZm8_T8P48'),												--play_date_id = 5
 	('Cinder Birthday Get Together', 6, 6, 4, GETDATE(),'1234 Main St Cleveland, OH 44111', 2,'ChIJ6dnrD4_7MIgRyDZm8_T8P48'),							--play_date_id = 6
@@ -76,22 +84,23 @@ VALUES ('City Park Play Date', 1, 1, 2, GETDATE(),'1234 Main St Cleveland, OH 44
 	('Welcome to the Neighborhood', 3, 3, 2, GETDATE(),'1234 Main St Cleveland, OH 44111', 2,'ChIJCRg1bEH8MIgR82sB2Njfn3M'),
 	('New Puppies', 3, 3, 2, GETDATE(),'2468 Maple Tree Ave Shaker Heights, OH 44001', 3,'ChIJCRg1bEH8MIgR82sB2Njfn3M'),
 	('Lets Play!', 3, 3, 2, GETDATE(),'9753 Doggie Drive Cleveland, OH 44111', 2,'ChIJCRg1bEH8MIgR82sB2Njfn3M'),
+	('Doggo playdate!', 2, 2, 5, GETDATE(),'9753 Doggie Drive Cleveland, OH 44111', 3,'ChIJCRg1bEH8MIgR82sB2Njfn3M'),
 	('New Dog Park Opening', 2, 1, 3, GETDATE(),'1357 Puppers Ave Rocky River, OH 44123', 1,'ChIJCRg1bEH8MIgR82sB2Njfn3M');	
 
 --populate test messages
 INSERT INTO play_date_messages (play_date_id, from_user_id, from_pet_id, post_date, message_text)
-VALUES (1, 1, 1, DATEADD(hour, -2, GETDATE()), 'This is a message from User!'),
-	(1, 2, 2, DATEADD(hour, -1, GETDATE()), 'Thanks for the play date!'),
-	(2, 2, 2, DATEADD(hour, -2, GETDATE()), 'This is a message from Admin!'),
-	(2, 5, 5, DATEADD(hour, -1, GETDATE()), 'Thanks for the play date!'),
-	(3, 3, 3, DATEADD(hour, -2, GETDATE()), 'This is a message from Mark!'),
-	(3, 2, 2, DATEADD(hour, -1, GETDATE()), 'Thanks for the play date!'),
-	(4, 4, 4, DATEADD(hour, -2, GETDATE()), 'This is a message from Zach!'),
-	(4, 1, 1, DATEADD(hour, -1, GETDATE()), 'Thanks for the play date!'),
-	(5, 5, 5, DATEADD(hour, -2, GETDATE()), 'This is a message from Ryan!'),
-	(5, 7, 7, DATEADD(hour, -1, GETDATE()), 'Thanks for the play date!'),
-	(6, 6, 6, DATEADD(hour, -2, GETDATE()), 'This is a message from Jacque!'),
-	(6, 4, 4, DATEADD(hour, -1, GETDATE()), 'Thanks for the play date!'),
+VALUES (1, 1, 1, DATEADD(hour, -2, GETDATE()), 'So excited for the playdate! See you then!'),
+	(1, 2, 2, DATEADD(hour, -1, GETDATE()), 'Thanks! Can''t wait!'),
+	(2, 2, 2, DATEADD(hour, -2, GETDATE()), 'Thanks for accepting the playdate request!'),
+	(2, 5, 5, DATEADD(hour, -1, GETDATE()), 'Such a good idea-can''t wait for our dogs to meet.'),
+	(3, 3, 3, DATEADD(hour, -2, GETDATE()), 'Hooray another playdate on the books!'),
+	(3, 2, 2, DATEADD(hour, -1, GETDATE()), 'Yay! Oh and remember to bring a water bottle this time.'),
+	(4, 4, 4, DATEADD(hour, -2, GETDATE()), 'Can''t wait to meet you and Bolt.'),
+	(4, 1, 1, DATEADD(hour, -1, GETDATE()), 'Same here for you and Raindance!'),
+	(5, 5, 5, DATEADD(hour, -2, GETDATE()), 'Thanks for joining-let me know if you need to update the time.'),
+	(5, 7, 7, DATEADD(hour, -1, GETDATE()), 'Great we''ll keep you posted.'),
+	(5, 5, 5, DATEADD(hour, -1, GETDATE()), 'Oh and let''s keep an eye on the weather-won''t be much fun if it rains!'),
+	(6, 6, 6, DATEADD(hour, -2, GETDATE()), 'Hope you accept the playdate request, would love to meet you!'),
 	(7, 7, 7, DATEADD(hour, -2, GETDATE()), 'This is a message from Kiran!'),
 	(7, 6, 6, DATEADD(hour, -1, GETDATE()), 'Thanks for the play date!'),
 	(8, 8, 8, DATEADD(hour, -2, GETDATE()), 'This is a message from David!'),
